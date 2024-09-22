31d17887027b9289a8d8da2401c52361ad6d4573
Almo2003 <164087169+Almo2003@users.noreply.github.com>
2024-09-22 09:51:20 -0500

    agregando vista privada

--- requirement/urls.py
+++ requirement/urls.py
@@ -21,5 +21,6 @@ from login import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
+    path('private/', views.private, name='private'),
 ]
