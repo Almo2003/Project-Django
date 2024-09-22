a9d59ecb1ce98234c4d15b5d0c883d483c38c4cf
Almo2003 <164087169+Almo2003@users.noreply.github.com>
2024-09-22 13:40:26 -0500

    agregacion y comprobacion del login

--- login/views.py
+++ login/views.py
@@ -1,47 +1,72 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponse
 from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def signup(request):
-    
     if request.method == 'GET':
-        return render(request, 'signup.html', {
-        'form': UserCreationForm
-        })
+        return render(request, 'signup.html', {"form": UserCreationForm})
     else:
-        if request.POST['password1'] == request.POST['password2']:
+
+        if request.POST["password1"] == request.POST["password2"]:
             try:
-                #register user
-                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(
+                    request.POST["username"], password=request.POST["password1"])
                 user.save()
                 login(request, user)
-                return redirect('private')
+                return redirect('signin')
             except IntegrityError:
                 return render(request, 'signup.html', {
-                'form': UserCreationForm,
-                'error': 'User Already Exist'
-                })
-            return render(request, 'signup.html', {
-                'form': UserCreationForm,
-                'error': 'Password do not match'
-                })
+                    "form": UserCreationForm, 
+                    "error": "Username already exists."})
 
+        return render(request, 'signup.html', {
+            "form": UserCreationForm, 
+            "error": "Passwords did not match."})
+
+@login_required
 def private(request):
     return render(request, 'private.html')
 
+@login_required
 def signout(request):
     logout(request)
     return redirect('home')
     
 def signin(request):
-       return render(request, 'signin.html')     
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm()
+        })
+    else:
+        # Autenticamos el usuario con username y password
+        user = authenticate(
+            request, 
+            username=request.POST['username'], 
+            password=request.POST['password']
+        )
+        if user is None:
+            # Si el usuario no es válido, lo redirigimos a la misma página con un error
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm(),
+                'error': 'Username o contraseña incorrectos.'
+            })
+        
+        # Si el usuario es válido, iniciamos sesión
+        login(request, user)
+        # Redirigimos a la vista 'private'
+        return redirect('private')
+            
+           
+             
+        
     
     
     
