93b67928c285a68060b31e1e7c629173fa8c220b
alvaro moreno <alvaromoreno.estudiante@iser.edu.co>
2024-12-05 14:29:10 -0500

    mejoras del signin y signup

--- requirement/settings.py
+++ requirement/settings.py
@@ -118,7 +118,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 LOGIN_URL = '/signin'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
